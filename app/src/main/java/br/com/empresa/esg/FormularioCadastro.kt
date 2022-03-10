882c91e3f55522130d6605a9706351febade5a4e
Lucas Siq <lucasdassiqueira@gmail.com>
2022-03-09 22:25:18 -0300

    fourth Commit

---
+++ app/src/main/java/br/com/empresa/esg/FormularioCadastro.kt
@@ -0,0 +1,11 @@
+package br.com.empresa.esg
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FormularioCadastro : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_formulario_cadastro)
+    }
+}
